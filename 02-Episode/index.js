98e9e21daa52b41b68182db93110306bb5d87e69
Deepak-nishad <deepakkumar62559@gmail.com>
2023-08-21 01:55:25 +0530

    episode 3 complete oloy last 20 minute left

--- 02-Episode/index.js
+++ 02-Episode/index.js
@@ -1,17 +1,43 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("H1", {}, "I am an h1 tag"),
-    React.createElement("H2", {}, "I am an h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("H3", {}, "I am an h3 tag"),
-    React.createElement("H4", {}, "I am an h4 tag"),
-  ]),
-]);
+// const heading = React.createElement("h1", { id: "hellooo" }, "namstay React");
+
+const heading = (
+  <h1 id="heding"> hello everone how are you i am jsx element </h1>
+);
+
+// Two types of components
+//class base components - old
+// functional components- new
+
+const TitleComponent = () => (
+  <div id="conatiner">
+    <h1 className="heading">Namstay react Title unctional compnent</h1>
+  </div>
+);
+
+// const HeadingComponent = () => (
+//   <div id="conatiner">
+//     <h1 className="heading">Namstay react functional compnent</h1>
+//   </div>
+// );
+
+//component composition
+// const HeadingComponent = () => (
+//   <div id="conatiner">
+//     <TitleComponent />
+//     <h1 className="heading">Namstay react functional compnent</h1>
+//   </div>
+// );
+
+// to paas react element in component
+const HeadingComponent = () => (
+  <div id="conatiner">
+    {heading}
+    <h1 className="heading">Namstay react functional compnent</h1>
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(parent);
+root.render(<HeadingComponent />);
